2c8295e403aa4160203cd1c96abb3fa8ca408340
Igor Krupenja <igor.krupenja@gmail.com>
2023-07-24 02:56:19 +0300

    Add markdown tests

--- src/utils/markdown.test.ts
+++ src/utils/markdown.test.ts
@@ -1,13 +1,49 @@
 import { beforeAll, describe, expect, it } from 'vitest';
 
-import { getCanonicalUrl } from './index.js';
+import {
+  getCanonicalUrl,
+  getCoverImageUrl,
+  insertCanonicalUrl,
+  insertCoverImage,
+} from './markdown.js';
 
 beforeAll(() => {
   import.meta.env.HASHNODE_URL = 'https://blog.IgorKrpenja.com';
+  import.meta.env.SUPABASE_URL = 'https://supabase.IgorKrpenja.com';
+  import.meta.env.SUPABASE_STORAGE_BUCKET = 'images';
 });
 
 describe('getCanonicalUrl', () => {
-  it('foo', () => {
+  it('should return correct URL', () => {
     expect(getCanonicalUrl('foo')).eq('https://blog.IgorKrpenja.com/foo');
+  });
+});
+
+describe('getCoverImageUrl', () => {
+  it('should return correct URL', () => {
+    expect(getCoverImageUrl('foo')).eq(
+      'https://supabase.IgorKrpenja.com/storage/v1/object/public/images/foo'
+    );
+  });
+});
+
+describe('insertCanonicalUrl', () => {
+  it('should return correct markdown', () => {
+    const markdown = 'foo';
+    const url = 'bar';
+    expect(insertCanonicalUrl(markdown, url)).eq(
+      '\n*This article was originally published on [my blog](bar).*\nfoo'
+    );
+  });
+});
+
+describe('insertCoverImage', () => {
+  it('should return correct markdown', () => {
+    const title = 'title';
+    const content = '## Heading';
+    const coverImagePath = 'path/to/image.jpg';
+    expect(insertCoverImage(title, content, coverImagePath)).eq(
+      '\n![title](https://supabase.IgorKrpenja.com/storage/v1/object/public/images/path/to/path/to/image.jpg)\n## Heading'
+    );
   });
 });
