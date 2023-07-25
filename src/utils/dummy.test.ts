3d3ac4f230e2a24ce8c22d57645af521c77ca797
Igor Krupenja <igor.krupenja@gmail.com>
2023-07-25 08:51:30 +0300

    Failing option 2

--- src/utils/dummy.test.ts
+++ src/utils/dummy.test.ts
@@ -4,7 +4,7 @@ import * as dummy from './dummy.js';
 
 describe('parent', () => {
   it('should return foobar', () => {
-    const spy = vi.spyOn(dummy, 'child').mockImplementationOnce(() => 'bar');
+    const spy = vi.spyOn(dummy, 'child').mockReturnValueOnce('bar');
     expect(dummy.parent()).eq('foobar');
     expect(spy).toHaveBeenCalled();
   });
