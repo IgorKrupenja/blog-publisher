ff39656ac23c4f3ba71236343cd8c5edf76d5b27
Igor Krupenja <igor.krupenja@gmail.com>
2023-07-26 20:02:05 +0300

    Improve test coverage config (#30)

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,10 +7,11 @@ export default defineConfig({
     globalSetup: './src/test/setup-tests.ts',
     reporters: ['verbose'],
     coverage: {
+      provider: 'istanbul',
       reporter: [['lcov', { projectRoot: './src' }], ['text']],
       all: true,
       include: ['**/src/**/*.ts'],
-      exclude: ['**/interfaces/*', '**/data/*', '**/test/*', '**/src/*.d.ts'],
+      exclude: ['**/interfaces/*', '**/data/*', '**/test/*', '**/src/*.d.ts', '**/src/*.index.ts'],
     },
   },
 });
