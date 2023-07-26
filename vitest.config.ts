9d5caa269c6fb2e2cddaec60175064ceda53ac6c
Igor Krupenja <igor.krupenja@gmail.com>
2023-07-26 19:25:42 +0300

    Add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,28 +5,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globalSetup: './src/test/setup-tests.ts',
+    reporters: ['verbose'],
     coverage: {
-      provider: 'v8', // or 'v8'
-      reporter: [['lcov', { projectRoot: './src' }], ['json', { file: 'coverage.json' }], ['text']],
-      // reporter: ['lcov'],
+      reporter: [['lcov', { projectRoot: './src' }], ['text']],
+      all: true,
+      include: ['**/src/**/*.ts'],
+      exclude: ['**/interfaces/*', '**/data/*', '**/test/*', '**/src/*.d.ts'],
     },
-    // globals: true,
-    // reporters: ['verbose'],
-    // coverage: {
-    //   all: true,
-    //   provider: 'v8',
-    //   reporter: ['text', 'html', 'lcov', 'cobertura', 'json'],
-    //   include: ['**/src/**/*.{js,jsx,ts,tsx}'],
-    //   exclude: [
-    //     '**/src/main.{js,jsx,ts,tsx}',
-    //     '**/*.types.{ts,tsx}',
-    //     '**/*.test.{js,jsx,ts,tsx}',
-    //     '**/src/vite-env*',
-    //   ],
-    //   statements: 0,
-    //   branches: 0,
-    //   functions: 0,
-    //   lines: 0,
-    // },
   },
 });
