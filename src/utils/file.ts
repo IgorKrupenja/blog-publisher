bd56d382d9233b5dff9a0ec790387290b409b686
Igor Krupenja <igor.krupenja@gmail.com>
2023-10-21 00:09:25 +0300

    Improve constant names in getArticle

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -7,21 +7,21 @@ import { Article, ArticleFrontMatter } from '../interfaces';
 export const getArticle = (path: string): Article => {
   if (!path) throw new Error('No article path provided.');
 
-  const markdown = fs.readdirSync(path).find((file) => file.endsWith('.md'));
+  const file = fs.readdirSync(path).find((file) => file.endsWith('.md'));
 
-  if (!markdown) throw new Error('getArticle: No markdown file found in article path.');
+  if (!file) throw new Error('getArticle: No markdown file found in article path.');
 
-  const parsedContent = matter(fs.readFileSync(`${path}/${markdown}`, 'utf-8'));
-  const frontMatter = parsedContent.data as ArticleFrontMatter;
+  const markdown = matter(fs.readFileSync(`${path}/${file}`, 'utf-8'));
+  const frontMatter = markdown.data as ArticleFrontMatter;
 
   if (!frontMatter.title) throw new Error('getArticle: No title found in article.');
   if (!frontMatter.tags) throw new Error('getArticle: No tags found in article.');
   if (!frontMatter.coverImage) throw new Error('getArticle: No cover image found in article.');
-  if (!parsedContent.content.length) throw new Error('getArticle: No content found in article.');
+  if (!markdown.content.length) throw new Error('getArticle: No content found in article.');
 
   return {
     ...(frontMatter as Required<ArticleFrontMatter>),
-    ...parsedContent,
+    ...markdown,
     coverImagePath: `${path}/${frontMatter.coverImage}`,
   };
 };
