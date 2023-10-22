213ce2319e32cf93a62e548ed1bb8e43296d46ae
Igor Krupenja <igor.krupenja@gmail.com>
2023-10-22 16:33:11 +0300

    Fix spy

--- src/utils/file.test.ts
+++ src/utils/file.test.ts
@@ -8,11 +8,13 @@ vi.mock('child_process', () => {
   };
 });
 
+import * as child_process from 'child_process';
+
 describe('getNewArticlePaths', () => {
   it('should return an array of new article paths', () => {
     // Mock the output of the `git diff` command
-    // const diffOutput = 'src/articles/2023/01/01-article.md\nsrc/articles/2023/02/02-article.md\n';
-    // const execSyncSpy = vi.spyOn(child_process, 'execSync').mockReturnValueOnce(diffOutput);
+    const diffOutput = 'src/articles/2023/01/01-article.md\nsrc/articles/2023/02/02-article.md\n';
+    const execSyncSpy = vi.spyOn(child_process, 'execSync').mockReturnValueOnce(diffOutput);
 
     // Call the function and expect the output
     expect(getNewArticlePaths()).toEqual([
@@ -21,9 +23,8 @@ describe('getNewArticlePaths', () => {
     ]);
 
     // Verify that the `git diff` command was called with the correct arguments
-    // expect(execSyncSpy).toHaveBeenCalledWith(
-    //   'git diff main HEAD --name-only -- "src/articles/**/*.md"',
-    //   expect.anything()
-    // );
+    expect(execSyncSpy).toHaveBeenCalledWith(
+      'git diff main HEAD --name-only -- "src/articles/**/*.md"'
+    );
   });
 });
