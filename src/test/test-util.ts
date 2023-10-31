0f0fdc55fcef78a25aa8ea5386431ddb9ef69e82
Igor Krupenja <igor.krupenja@gmail.com>
2023-11-01 00:13:21 +0200

    Better types

--- src/test/test-util.ts
+++ src/test/test-util.ts
@@ -1,8 +1,11 @@
+import { AnyFunction } from 'bun';
 import { Mock, expect } from 'bun:test';
 
 // TODO: Bun does not support .toHaveBeenCalledWith() yet, https://github.com/oven-sh/bun/issues/1825
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const expectToHaveBeenCalledWith = (mock: Mock<any>, expectedArgs: unknown[]): void => {
+export const expectToHaveBeenCalledWith = (
+  mock: Mock<AnyFunction>,
+  expectedArgs: unknown[]
+): void => {
   const calls = mock.mock.calls;
   const matchingCall = calls.find((call) => call.some((arg) => expectedArgs.includes(arg)));
   expect(matchingCall).toBeDefined();
