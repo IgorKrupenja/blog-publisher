22382ce3ebe11b096773a853dce4c2dbc5ec3ec7
Igor Krupenja <igor.krupenja@gmail.com>
2023-11-01 00:12:38 +0200

    Revert "WIP publish test"
    This reverts commit 8d4c014d5b5fe372b007c56502de00ad3a3dc900.

--- src/test/test-util.ts
+++ src/test/test-util.ts
@@ -1,11 +1,8 @@
-import { AnyFunction } from 'bun';
 import { Mock, expect } from 'bun:test';
 
 // TODO: Bun does not support .toHaveBeenCalledWith() yet, https://github.com/oven-sh/bun/issues/1825
-export const expectToHaveBeenCalledWith = (
-  mock: Mock<AnyFunction>,
-  expectedArgs: unknown[]
-): void => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const expectToHaveBeenCalledWith = (mock: Mock<any>, expectedArgs: unknown[]): void => {
   const calls = mock.mock.calls;
   const matchingCall = calls.find((call) => call.some((arg) => expectedArgs.includes(arg)));
   expect(matchingCall).toBeDefined();
