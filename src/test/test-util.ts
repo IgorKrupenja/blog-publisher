f3cddae8abfd196097bcb88c608cf2d46bed1c5f
Igor Krupenja <igor.krupenja@gmail.com>
2023-11-01 20:02:45 +0200

    Simplify expectToHaveBeenCalledWith

--- src/test/test-util.ts
+++ src/test/test-util.ts
@@ -2,38 +2,22 @@ import { AnyFunction } from 'bun';
 import { Mock, expect } from 'bun:test';
 
 // TODO: Bun does not support .toHaveBeenCalledWith() yet, https://github.com/oven-sh/bun/issues/1825
-// todo try proper types?
 export const expectToHaveBeenCalledWith = (
   mock: Mock<AnyFunction>,
   expectedArgs: unknown[]
 ): void => {
   const calls = mock.mock.calls;
-  // console.log(calls);
-  // const matchingCall = calls.find((call) => call.some((arg) => expectedArgs.includes(arg)));
+  expect(calls.length).toBeGreaterThan(0);
 
-  let matchingCall;
-  for (const call of calls) {
-    for (const arg of call) {
-      let isArgIncluded = false;
-      for (const expectedArg of expectedArgs) {
-        if (typeof expectedArg !== 'object' && expectedArg === arg) {
-          isArgIncluded = true;
-          break;
-        } else {
-          isArgIncluded = Bun.deepEquals(expectedArg, arg);
-        }
-      }
-
-      if (isArgIncluded) {
-        matchingCall = call;
-        break;
-      }
-    }
-
-    if (matchingCall) {
-      break;
-    }
-  }
+  const matchingCall = calls.find((call) =>
+    call.some((arg) =>
+      expectedArgs.some((expectedArg) =>
+        typeof expectedArg !== 'object' && typeof expectedArg !== 'function'
+          ? expectedArg === arg
+          : Bun.deepEquals(expectedArg, arg)
+      )
+    )
+  );
 
   expect(matchingCall).toBeDefined();
 };
