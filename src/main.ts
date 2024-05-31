6e739e407a0c3196078a504c3a32cabf819fcce4
Igor Krupenja <igor.krupenja@gmail.com>
2024-05-31 14:35:35 +0300

    Add debug logic

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import { publishArticle } from './utils/publish';
 
 export const publishArticles = async (): Promise<void> => {
   const paths = getNewArticlePaths();
+
+  console.log(paths);
+
   if (!paths.length) {
     console.debug('publishArticles: No new articles to publish.');
     return;
   }
 
+  console.log('new are there');
+
   await Promise.all(paths.map((path) => publishArticle(path)));
+
+  console.log('published');
 };
 
 await publishArticles();
